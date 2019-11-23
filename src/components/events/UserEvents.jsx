778a6b7bfc8dc8770f6eae51e9f5debec4e5bc37
randolphjr7 <randolphjr7@gmail.com>
2019-11-23 09:37:01 -0500

    bootstrap carousel

--- src/components/events/UserEvents.jsx
+++ src/components/events/UserEvents.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
+import Carousel from 'react-bootstrap/Carousel'
+import CarouselCaption from "react-bootstrap/CarouselCaption";
 
 let filterEvents = [];
 class UserEvents extends Component {
@@ -37,10 +39,25 @@ class UserEvents extends Component {
     // console.log(filterEvents);
     return filterEvents.map(eachEvent => {
       return (
-        <div key={eachEvent._id}>
-          <img src={eachEvent.imageUrl} alt="boohoo" height="300" />
-          <h3>{eachEvent.eventName}</h3>
-        </div>
+        
+  
+          <Carousel.Item>
+              <img 
+                key={eachEvent._id}
+                className="d-block w-100"
+                src={eachEvent.imageUrl} 
+                alt="boohoo" 
+        /*         max-width="100%"
+                max-height="100%" */
+                height="600" 
+              />
+              <CarouselCaption>
+                <h3>{eachEvent.eventName}</h3>
+              </CarouselCaption>
+            
+          </Carousel.Item>
+     
+
       );
     });
   };
@@ -49,10 +66,30 @@ class UserEvents extends Component {
     return (
       <div>
         <h1>My Events:</h1>
-        {this.myUserEvents(this.state.myId)}
+        <Carousel>
+          {this.myUserEvents(this.state.myId)}
+        </Carousel>
       </div>
     );
   }
 }
 
 export default UserEvents;
+
+
+
+{/* <Carousel>
+<Carousel.Item>
+    <img 
+      key={eachEvent._id}
+      className="d-block w-100"
+      src={eachEvent.imageUrl} 
+      alt="boohoo" 
+      height="300" 
+    />
+    <CarouselCaption>
+      <h3>{eachEvent.eventName}</h3>
+    </CarouselCaption>
+  
+</Carousel.Item>
+</Carousel> */}
